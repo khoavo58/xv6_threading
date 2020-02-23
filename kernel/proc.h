19c3e85580f4ed7d7de02206bc69cd5e8525929b
khoa vo <khoavo58@github.com>
2020-02-22 17:46:32 -0800

    added variable thread_count and tid and stack.

--- kernel/proc.h
+++ kernel/proc.h
@@ -97,10 +97,17 @@ struct proc {
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Bottom of kernel stack for this process
   uint64 sz;                   // Size of process memory (bytes)
-  pagetable_t pagetable;       // Page table
+  pagetable_t pagetable;       // Page table 
   struct trapframe *tf;        // data page for trampoline.S
   struct context context;      // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  //ADDED VARIABLES
+  void* stack;
+  int thread_count;             
+  int tid;                      //thread ID
+
+
 };
